51578c5457bd3f882a354f7c8b07e1cd549069b1
Nga Tran <nga.tran@it-development.com>
2019-12-29 22:38:39 +0700

    #D Implement butterfly and caterpillar

---
+++ src/main/java/com/media/assignment/Butterfly.java
@@ -0,0 +1,9 @@
+package com.media.assignment;
+
+class Butterfly extends Bird {
+
+	@Override
+	void say() {
+		System.out.println("I cannot say");
+	}
+}
---
+++ src/main/java/com/media/assignment/Caterpillar.java
@@ -0,0 +1,9 @@
+package com.media.assignment;
+
+class Caterpillar extends Bird {
+
+	@Override
+	void fly() {
+		System.out.println("I cannot fly");
+	}
+}
